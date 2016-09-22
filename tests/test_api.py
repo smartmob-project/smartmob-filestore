b557f1c9f8a8f734f666c9be39bfe80b21358fbc
Andre Caron <andre.l.caron@gmail.com>
2016-09-21 20:26:49 -0400

    Adds environment variable for controlling logging endpoint.

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 
 
+import asyncio
 import aiohttp
 import os
 import pytest
 import signal
 
 from smartmob_filestore import main
-from unittest import mock
+from timeit import default_timer
 
 
 @pytest.mark.asyncio
 async def test_upload_and_download(event_loop, unused_tcp_port_factory,
-                                   tempdir, fluent_server):
+                                   tempdir):
     """Uploaded files can be downloaded again."""
 
     # Start the server.
@@ -23,10 +24,6 @@ async def test_upload_and_download(event_loop, unused_tcp_port_factory,
     task = event_loop.create_task(main([
         '--host=%s' % host,
         '--port=%d' % port,
-        '--logging-endpoint=fluent://%s:%d/smartmob-filestore' % (
-            fluent_server[0],
-            fluent_server[1],
-        ),
     ], loop=event_loop))
 
     async with aiohttp.ClientSession(loop=event_loop) as client:
@@ -35,13 +32,16 @@ async def test_upload_and_download(event_loop, unused_tcp_port_factory,
         # Upload the file.
         #
         # NOTE: it may take a moment for the server to become ready.
-        while True:
+        ref = default_timer()
+        now = default_timer()
+        while (now - ref) < 5.0:
             try:
                 async with client.put(url, data=b'Hello, world!') as response:
                     assert response.status == 201
                 break
             except aiohttp.errors.ClientOSError:
-                pass
+                await asyncio.sleep(0.1)
+            now = default_timer()
 
         # Download it back.
         async with client.get(url) as response:
@@ -54,22 +54,3 @@ async def test_upload_and_download(event_loop, unused_tcp_port_factory,
 
     # Make sure we got what we uploaded.
     assert content == b'Hello, world!'
-
-    # Access should have been logged.
-    print(fluent_server[2])
-    assert fluent_server[2] == [
-        [b'smartmob-filestore.http.access', mock.ANY, {
-            b'@timestamp': mock.ANY,
-            b'request': mock.ANY,
-            b'duration': mock.ANY,
-            b'outcome': 201,
-            b'path': b'/hello.txt',
-        }],
-        [b'smartmob-filestore.http.access', mock.ANY, {
-            b'@timestamp': mock.ANY,
-            b'request': mock.ANY,
-            b'duration': mock.ANY,
-            b'outcome': 200,
-            b'path': b'/hello.txt',
-        }],
-    ]
